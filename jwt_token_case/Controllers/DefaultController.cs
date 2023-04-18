b2a5ccd6dd89e63f517f8511bfe9b5e546644883
Erencem <82844580+erencemozturk@users.noreply.github.com>
2023-04-18 10:01:34 +0300

    Add files via upload

---
+++ jwt_token_case/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using jwt_token_case.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_token_case.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+           
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Page1()
+        {
+            return Ok("Sayfa 1 için giriş başarılı.");
+        }
+        
+    }
+}
